08c2d6551762a4ad029dbf77981e02d1272385f0
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 00:17:04 -0600

    used bracket notation after lastName line 15

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,15 @@
+//learned javascript starts counting at 0 instead of one and how to use bracket notation to find characters at specific indexes of strings
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+//added [0] to find first character of variable last name
+firstLetterOfLastName = lastName[0];
