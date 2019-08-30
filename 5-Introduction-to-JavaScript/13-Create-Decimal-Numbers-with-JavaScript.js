7ecbb6d2385b4a29b3eab11f303069a3ccfddd21
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:54:15 -0600

    declared and initialized myDecimal

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,8 @@
+//learned we can store decimals in variables, sometimes called floating point numbers
+
+//declared a variable myDecimal and initialized it with a value of 4.2
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 4.2;
