9800d90b0b5370422a516f1ab2a6a0ce87a80290
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 11:58:16 -0600

    added "Kayla" and 35 to myArray

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,8 @@
+//learned about arrays and how they can be used to store multiple variables in one value and they can even be different date types
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+//added the string Kayla and the number 35 to myArray
+var myArray = ["Kayla", 35];
