eca30d0dad2db5d5d6d8814b617319ffefd3e643
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:41:11 -0600

    completed on wrong lesson. converted variables a,b,c to use augmented multiplication

--- 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -1,13 +1,13 @@
-//learned -= can be used just like += but to subtract instead of add
+//learned how to multiply a variable using *=
 
-//converted the assignments of var a,b,c to use -= so a = 11 - 6; becomes a -= 6; ect..
+//convertd the assignments for a,b,c to use *= so a = a * 5; becomes a *= 5; ect...
 
-var a = 11;
-var b = 9;
-var c = 3;
+var a = 5;
+var b = 12;
+var c = 4.6;
 
 // Only modify code below this line
 
-a -= 6;
-b -= 15;
-c -= 1;
+a *= 5;
+b *= 3;
+c *= 10;
