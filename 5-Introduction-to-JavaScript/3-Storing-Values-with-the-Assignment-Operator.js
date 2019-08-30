fd4b4aee5a02d643576a772906a8e767bc016ab2
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:13:04 -0600

    assigned value to variables a and b

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,11 @@
+//learned how to assign values to variables and how the value of a variable can be reassigned to another variables
+
+//assigned the value 7 to variable a and assigned the value of a to variable // BUG:
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
