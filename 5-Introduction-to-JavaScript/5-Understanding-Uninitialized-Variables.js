204563d51b929ed583a3fed9b9baf15cbe770daa
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:20:33 -0600

    Initialized var a, b and c

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,15 @@
+/* learned how variables have an initial value of undefined when declared. Mathmatical operations on undefinded variables
+result in NaN (not a number), concatenation of a string with an undefined variable will give you a literal string of undefined*/
+
+//initialized var a to 5, b to 10 and c to "I am a"
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
