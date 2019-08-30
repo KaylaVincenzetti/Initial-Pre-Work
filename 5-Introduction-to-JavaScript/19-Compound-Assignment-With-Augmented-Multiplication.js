4157ddbe00f2f6cc68b846b98a93a7d01a625630
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:13:27 -0600

    converted assignments of variables a,b,c to use -=

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,13 @@
+//learned -= can be used just like += but to subtract instead of add
+
+//converted the assignments of var a,b,c to use -= so a = 11 - 6; becomes a -= 6; ect..
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
