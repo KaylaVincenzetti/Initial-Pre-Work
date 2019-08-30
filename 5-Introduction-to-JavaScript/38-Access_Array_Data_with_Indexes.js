a5546a8d6a16f5194a8df72e0ceee2c079dbee4a
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:06:16 -0600

    declared variable myData and initialized it's value line 12

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,12 @@
+//learned to access data inside an array using indexes
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+declared variable myData and initialized it's value to be that of myArray index 0
+var myData = myArray[0];
