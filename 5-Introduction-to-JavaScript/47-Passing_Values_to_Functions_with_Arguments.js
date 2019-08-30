50fe356af7c64499074f32d971763017f2f4b431
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 13:30:55 -0600

    created function functionWithArgs

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+//learned about passing values into functions. Parameters are placeholder variables. Arguments are values passed into function when called
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+//created function with two parameters num1 and num2. Passed in arguments fo 23 and 9 when function was called
+function functionWithArgs(num1, num2) {
+  console.log(num1 + num2);
+}
+
+functionWithArgs(23, 9);
