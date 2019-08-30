eb796511e92b72e27150f0dd16a887bf96485388
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:47:49 -0600

    declared two variables and assigned strings to each

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,11 @@
+//learned how to assign variables string values. 0 or more characters enclosed in quotes is called a string literal
+
+//created two variables and assigned the strings Kayla and Vincenzetti to them
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Kayla";
+var myLastName = "Vincenzetti";
