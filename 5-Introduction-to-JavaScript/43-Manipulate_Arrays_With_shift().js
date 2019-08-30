0b7a54f8ddaf14fbb93fedd66eeb82212ea881e4
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:27:44 -0600

    removed first item from myArray with .shift() line 13

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,13 @@
+//learned .shift() works just like .pop() but removes the first item from an array
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+//removed the first item form myArray 
+var removedFromMyArray =myArray.shift();
