2a80abf2e41af610cc85cfd68991434f7cb8914e
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 00:26:15 -0600

    altered code line 12 to return last letter of lastName

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,12 @@
+//learned subtracting one from a strings length will retunr last letter of strings
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+//altered code to return last letter of lastName
+var lastLetterOfLastName = lastName[lastName.length - 1];
