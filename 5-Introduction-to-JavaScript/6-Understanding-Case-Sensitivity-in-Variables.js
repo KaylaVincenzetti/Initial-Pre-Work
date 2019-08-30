138f2483cac9af6e3992238aeb32c00c81d6e4e1
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:26:44 -0600

    updated declarations and assignments to camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,14 @@
+/* learned it's possible to have multiple variables with the same name but differnet casing. This in not recommended
+the preferred method is to use camelCase where the first word is lower case and each word after starts with an upper case letter */
+
+//changed the casing on the declerations and assignments to camelCase
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
