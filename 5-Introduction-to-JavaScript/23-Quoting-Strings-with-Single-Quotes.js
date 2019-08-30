461aa591cfa682274cba7127ba2d7934ce29e18a
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:56:41 -0600

    used backslashes to designate quotes within string assigned to myStr

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,5 @@
+//learned how to use quotes inside a javascript string
+
+//used backslashes to ensure parts of string in quotes woule appear in quotes instead of designating the end of the string
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
