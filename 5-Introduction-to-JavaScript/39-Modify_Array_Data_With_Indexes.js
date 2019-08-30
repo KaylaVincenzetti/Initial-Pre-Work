8751bef2ada58f804749c1ebdb271c8a0829b7b0
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:09:27 -0600

    updated the value of index 0 in myArray line 12

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+//learned how entries in arrays can be changed unlike strings
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+//set myArray to replace 18 at index 0 with 45
+myArray[0] = 45;
