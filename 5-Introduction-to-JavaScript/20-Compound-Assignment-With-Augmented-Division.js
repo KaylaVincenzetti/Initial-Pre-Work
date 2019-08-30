7d28abd08a3dd604bd75c6eeefa48555a436f3e2
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:16:19 -0600

    converted variables a,b,c to use *= operator

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,13 @@
+//learned how to multiply a variable using *=
+
+//convertd the assignments for a,b,c to use *= so a = a * 5; becomes a *= 5; ect...
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
