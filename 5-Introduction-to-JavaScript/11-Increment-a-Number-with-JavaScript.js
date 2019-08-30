6808ea2829f1a4573f12e9558fe85367311dc828
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:46:26 -0600

    changed myVar = myVar + 1; to myVar++

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,8 @@
+//learned how to add 1 to a variable with ++
+
+//changed myVar = myVar + 1; to myVar++ because it's the same thing
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
