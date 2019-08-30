87708215c16dc6bfa7b9ddfa2f0be5ff9f63ece0
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:04:11 -0600

    set remainder of 11 divided by 3 var ramainder

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,7 @@
+// learned about the remainder operator % that gives the remainder of the division of 2 numbers
+
+//set the remainder to equeal the difference of 11 divided by 3
+
+// Only change code below this line
+
+var remainder = 11 % 3;
