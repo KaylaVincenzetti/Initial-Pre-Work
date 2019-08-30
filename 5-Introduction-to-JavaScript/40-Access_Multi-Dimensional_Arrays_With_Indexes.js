5689ad019640a295f0f345c1355f84eaf9482320
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:16:10 -0600

    updated code on line 8 so myData would return 8

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,8 @@
+//learned to access data within multidimensional arrays (arrays within an array)
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+//set myData to return 8 which is index 2 of my array and index 1 of index 2
+var myData = myArray[2][1];
