dd4126b98eee0be92914c7a73ac5abcd95933591
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:37:38 -0600

    changed 0 to 33 to get the quotient of 2

---
+++ 5-Introduction-to-JavaScript/10-Divide-One-Number-by-Another-with-JavaScript.js
@@ -0,0 +1,5 @@
+//learned to divide numbers using /
+
+//changed the 0 to 33 to get the quotient of 2. Declared variable quotient and assigned it the value of 66 divided by 33
+
+var quotient = 66 / 33;
