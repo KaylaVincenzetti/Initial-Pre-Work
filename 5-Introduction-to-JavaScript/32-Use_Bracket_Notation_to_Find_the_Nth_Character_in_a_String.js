d5d643edd4e4c0a983d3293d7c2f6a861d69cadc
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 00:23:25 -0600

    updated code to return 3rd letter of lastName variable line 11

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//used bracket notaition to find 3rd letter of lastName variable
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
