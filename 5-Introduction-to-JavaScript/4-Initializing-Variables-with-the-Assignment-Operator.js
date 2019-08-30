971691cba3020d2e91ec6ea9d356659e29c79a17
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 21:15:39 -0600

    declared variable a and set initial value to 9

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+//learned about initializing variables with a value within the same line it was originally declared
+
+//declared variable a and initialized it with a value of 9
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
