4fa23786b0bacfa0a07aa6061e11a0d29b94b878
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 00:20:39 -0600

    removed [0] after myStr and changed "H" to "Hello World"

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,10 @@
+//learned individual characters of strings cannot be altered and to change you would have to change entire strings
+
+//Fixed code to say Hello World
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
