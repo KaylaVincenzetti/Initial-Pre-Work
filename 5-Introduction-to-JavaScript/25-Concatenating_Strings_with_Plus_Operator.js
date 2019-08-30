9925fd265056c2e578edbcfdcc3084f659ee00ae
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:09:35 -0600

    added escape sequences to string to format string correctly when printed to console

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,5 @@
+//learned about the many different types of escape sequences
+
+//used escape sequences to format this string correctly when it prints to the console
+
+var myStr = "FirstLine\n\t\\SecondLine\n\ThirdLine"; // Change this line
