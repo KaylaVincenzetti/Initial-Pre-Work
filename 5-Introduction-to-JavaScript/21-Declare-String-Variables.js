b5e36b4e941c189703ec6085fed051993d6eb4ac
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 22:42:58 -0600

    converted variables a,b,c to use /= operator

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,13 @@
+//learned we can use /= to divide variables by another number
+
+//converted assignments for a,b,c to use /=. a = 48 / 12; becomes a /= 12; ect...
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
