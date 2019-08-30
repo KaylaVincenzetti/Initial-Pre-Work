f268f4ffbe077954bb45604dbd5ae658e66be27f
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 13:12:53 -0600

    created function called reusableFunction, set it to print "Hi World" to console when called

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,16 @@
+//learned we can create code to be reused multiple times with functions
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+//created a function called reus and set it to print "Hi World"  to the console when function is called
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
