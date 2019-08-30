47eba36ae7eada60bb3705f05b69eb4329ff1539
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:03:21 -0600

    updated string to use single quotes start/finish removed escape(\) characters

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,5 @@
+//learned we can enclose strings in eiher single or double quotes but they should always start and end with the same type
+
+//updated myStr to start and end with single quotes so doulbe quotes could be used inside with no escape(\) characters
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';)
