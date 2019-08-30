e4f0f3ef5a044339eec4e9316c9d1e7bb95487b7
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:14:06 -0600

    used concatenation to combine two strings in myStr

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,8 @@
+//learned to add strings together using the + operator, this is called concatenation
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+//concatenated these two strings to turn the two strings into one
+var myStr = "This is the start." + " This is the end.";
