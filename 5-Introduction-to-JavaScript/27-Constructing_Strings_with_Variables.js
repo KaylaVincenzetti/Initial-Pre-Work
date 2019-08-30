2a959c53b73c9fdce3b268d35f47689ec6f8f592
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:17:26 -0600

    concatenated the two strings using += in myStr

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+//learned how to use += to concatenate a string onto the end of a string
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+//used += to concatenate the two strings
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
