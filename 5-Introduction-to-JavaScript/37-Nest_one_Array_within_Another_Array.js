4200b302b47543aad8b49ba77753debdba608465
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:02:47 -0600

    added nested array inside of myArray

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,8 @@
+//learned to create nested arrays, arrays within arrays
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+//added a nested array to myArray
+var myArray = [["Kayla", 35], ["Bill", 43]];
