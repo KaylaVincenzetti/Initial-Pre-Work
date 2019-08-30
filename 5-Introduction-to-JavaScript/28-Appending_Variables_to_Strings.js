ef7a09b6275bebe543bc38f0e381b7452402ad16
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:22:30 -0600

    set value of myName and built myStr

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,10 @@
+//learned to insert variables into string by using + operator
+
+//built myStr by concatenating with myName variables
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Kayla";
+var myStr = "My name is " + myName + " and I am well!";
