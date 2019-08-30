8f40cd050702cfd52c11bc6e066c97cc3f85329e
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-29 23:26:41 -0600

    appended someAdjective to myStr

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,12 @@
+//learned to append variables to strings using +=
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+//appended someAdjective to myStr to create the string "Learning to code is Fantastic!"
+var someAdjective = "fantastic!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
