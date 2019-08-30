529406cf2c9695f79f53a2f1ec9ad103e65a7a26
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:21:20 -0600

    pushed dog, 3 to end of myArray

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,13 @@
+//learned to append data to an array using .push, this essentially pushes new items to end of an array
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+pushed dog, 3 to end of myArray
+myArray.push(["dog", 3]);
