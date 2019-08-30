4ee89c354654b0982143c5dc637bfa2e7514aeb8
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 12:25:10 -0600

    used .pop() to remove the last value from myArray

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,13 @@
+//learned how to remove a value from the end of an arrya using .pop()
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+//removed and returned last value of myArray
+var removedFromMyArray = myArray.pop();
