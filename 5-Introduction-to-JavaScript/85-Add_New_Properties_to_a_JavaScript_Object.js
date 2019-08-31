f99e1b55cd0a84054dd8bf39d4addf854de97603
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:25:49 -0600

    added the bark property to myDog object and set it to "woof"

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+//learned how to add properties to existing objects using . or [] notation
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+//added the bark property to myDog object and set it to "woof"
+myDog.bark = "woof";
