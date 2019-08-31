6402c733a333e3f5e2e1f413648a0095bdbadaac
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 15:10:01 -0600

    set up the for loop to increment by 2 each iteration and to push odd numbers between 1-9 into myArray

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+//learned that for loops don't have to iterate one at a time and can increment by 2 ect...
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+//set up the for loop to increment by 2 each iteration and to push odd numbers between 1-9 into myArray
+var myArray = [];
+for(var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+// Only change code below this line.
