6f4b7353089309a68251906c73b95cd388fd7bd8
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:13:39 -0600

    used bracket notation to access set variables on lines 12 and 13 to their corresponding object property in testObj

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,14 @@
+//learned about accessing object properties with bracket notation, this must be used if property name has a space in it, also property name must be in quotes when there is a space
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+//set entreeValue to equal "an entree" property of testObj
+var entreeValue = testObj["an entree"];   // Change this line
+//set drinkValue to equal "the drink" property of testObj
+var drinkValue = testObj["the drink"];    // Change this line
