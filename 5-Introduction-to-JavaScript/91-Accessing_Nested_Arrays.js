0e48af734cce0c4d5ee08fcae5fb2a45998e91a0
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 13:49:56 -0600

    retreived the second tree from the myPlants object

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+//learned how to use array bracket notation to access nested arrays within objects
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+//retreived the second tree from the myPlants object
+var secondTree = myPlants[1].list[1]; // Change this line
