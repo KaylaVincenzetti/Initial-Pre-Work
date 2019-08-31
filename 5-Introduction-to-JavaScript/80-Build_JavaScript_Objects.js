1793d5093cf7a314fcf9c688110a6df4f16b2bb3
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:04:32 -0600

    created myDog object

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+//learned about javascript objects which are similar to arrays but we acces their data by properties not indexes
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+//made an object called mydog
+var myDog = {
+  "name": "Corbin",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Tuco", "Arjune", "Trishia", "Thor"]
+
+
+
+
+};
