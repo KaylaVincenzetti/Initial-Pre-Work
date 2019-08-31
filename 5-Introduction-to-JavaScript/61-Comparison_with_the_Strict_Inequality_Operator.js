0a10514442f8c400d03ee9c2d13e9ee96253b029
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 20:39:05 -0600

    Set up function to return not equal if val is not equal to 17 type and value

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+//learned about the strict inequality operator which works the same as the strict equality operator but for things that do not have the same value and type
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+  //set strict inequality for val and 17
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
