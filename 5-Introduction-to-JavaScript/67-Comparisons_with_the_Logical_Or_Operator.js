47ad1f012477149f3b5f4226195cb3e766cd840a
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 23:04:58 -0600

    updated code to use logical Or operator on line 6

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,16 @@
+//learned how to use the logicial or operator, this will return true if either opperand is true
+
+function testLogicalOr(val) {
+  // Only change code below this line
+//updated code to use logicial Or operator to check if val is greater than 20 or less than 10
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
