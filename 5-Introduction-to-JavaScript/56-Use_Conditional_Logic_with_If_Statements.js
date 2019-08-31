374d6fdd9bcb673d04fafbb5e1b6085553ac5fa4
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 20:14:05 -0600

    created the if statement on lines 17 - 19

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,27 @@
+//learned about if statements and how they will be ran only if certain conditions are met
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  //created if statement that will return "Yes, that was true" if true or "No, that was false" if false
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
