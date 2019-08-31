e3a5ca625f4e538155abb4c85ec9315954ab8192
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 20:21:55 -0600

    changed line 7 to compare val to 12

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,14 @@
+//learned about equality operators, thay compare two values and will return true if they are equal, otherwise returns false
+
+
+// Setup
+function testEqual(val) {
+  //changed this line of code to check if val is equal to 12
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
