9bc8dc42fa7a2106bc2510f538207623e5b08425
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:20:44 -0600

    set playerNumber to 16 and set player to access player number property in testObj object

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,14 @@
+//bracket notation can also be used to access a property that is being stored as the value of a variable
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+//set to equal player number 16 in testObj
+var playerNumber = 16;       // Change this Line
+//sett to access property of players number in testObj
+var player = testObj[playerNumber];   // Change this Line
