f177bbdd704323bab2de7d739260a4bc7565d4bd
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 15:05:46 -0600

    created a for loop to push the nunmbers 1-5 into myArra

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+//learned about for loops and how they will run the same block of code multiple times until a specified condition is met
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+//created a for loop to push the nunmbers 1-5 into myArray
+var myArray = [];
+for(var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+
+// Only change code below this line.
