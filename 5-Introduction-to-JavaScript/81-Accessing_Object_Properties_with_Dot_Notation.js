c68e1a19d3462268468888085bcd288e620cf4f6
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:08:38 -0600

    set hatValue and shirtValue to equal hat and shirt property in testObj

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,14 @@
+//learned to access properties of an object using .notation
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+//set hatValue to equal hat property in testObj
+var hatValue = testObj.hat;      // Change this line
+//set shirtValue to equal shirt property in testObj
+var shirtValue = testObj.shirt;    // Change this line
