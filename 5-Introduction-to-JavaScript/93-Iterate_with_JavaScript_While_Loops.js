80c8ac8f65bfe3544a93eca73db1434fc91eeb90
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 14:55:39 -0600

    created a while loop to push the numbers 1-4 into myArray

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+//learned about while loops and how they only run while a specified condition is true and stops running once condition is no longer true
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+//created a while loop to push the numbers 1-4 into myArray
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++
+}
