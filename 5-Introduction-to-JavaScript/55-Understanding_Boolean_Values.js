99fc6f7aaa5ad58c359f33cbea5d2d10538a7388
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 19:58:26 -0600

    modified line 7 to return true instead of false

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+//learned about booleans data types and how they only return true or false
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+//modified line 7 to return true instead of false
+return true; // Change this line
+
+// Only change code above this line.
+}
