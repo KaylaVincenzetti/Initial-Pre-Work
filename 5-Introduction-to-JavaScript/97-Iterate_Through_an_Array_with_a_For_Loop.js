4ce3293ecba9055d8485f15f74dae22b50928652
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 15:25:22 -0600

    declared and initialized variable "total" and set up for loop to iterate through array

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,20 @@
+//learned how to iterate through an array with a for loop
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+//declared and initalized total variable
+var total = 0;
+//created a for loop to iterate through myArr
+for(var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+// Only change code below this line
