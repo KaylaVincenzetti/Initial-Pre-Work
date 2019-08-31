1767cf3049a0cb6fcec6e83151f89d76d6b91bec
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 12:59:11 -0600

    added a new ablum to the myMusic array

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,28 @@
+//learned a javascript object is a good way to store data in a flexable data structure
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  //added a new ablum to the myMusic array
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+    "gold": true
+  }
+];
