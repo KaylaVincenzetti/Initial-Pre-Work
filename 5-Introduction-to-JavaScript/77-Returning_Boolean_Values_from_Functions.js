cde85b1aa23cb40e4a784a6871b5df4abb6545bc
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 10:43:36 -0600

    removed if statement and replaced with comparison operator inside return

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+//learned we don't need to use if statments to do comparisons if the value will return true/false
+//removed if statments and set up to return true of false with use of comparison operator
+function isLess(a, b) {
+  // Fix this code
+
+    return a < b;
+
+}
+
+// Change these values to test
+isLess(10, 15);
