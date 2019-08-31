0496a1110b2b03ed333afaa382f5acf106b8c0af
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 19:00:58 -0600

    created timesFive function lines 10 - 12 and wrote console.log on line 16

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+//learned how to use function returns to return value from an argument we pass in
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+//created function timesFive passed in num as parameter, set return to return num times 5, passed in 10 as argument in console.log
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
+console.log(timesFive(5));
