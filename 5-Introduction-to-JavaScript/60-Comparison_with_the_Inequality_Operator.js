b51025269d8f81fc1ee4e32fec2e8db63bb6b22b
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-30 20:35:43 -0600

    updated code with inequality operator so function would return not equal if val isn't equal to 99

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,13 @@
+//learned about the inequality operator and how it will also convert data types like the equality operator
+
+// Setup
+function testNotEqual(val) {
+  //set to return not equal if val doesn;t equal 99 with inequality operator !=
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
