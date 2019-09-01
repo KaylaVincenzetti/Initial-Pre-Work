e75456a885e5c91b973241270b261c117372cab1
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 18:02:09 -0600

    changed while loop to do while loop

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+//learned how to create a do while loop which is like a while loop but it will do one pass of the code initially no matter what and then stop once condition is no longer true
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+
+  myArray.push(i);
+  i++;
+} while (i < 5)
