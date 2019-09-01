6a4bf0d7b5ecea0e6e226e6c40c2125870f01a4b
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 19:14:50 -0600

    used Math.floor and Math.random to generate random numbers between myMin and myMax

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,20 @@
+//learned to generate random numbers that fall between a range of two numbers
+
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+//used Math.floor and Math.random to generate random numbers between myMin and myMax
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
