c5908e6d472e8d59f219abac1d01a63770f0720c
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 18:25:44 -0600

    created a while loop and used Math.random to return a random number instead of 0

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,14 @@
+//learned about the Math.random function
+
+function randomFraction() {
+
+  // Only change code below this line.
+    var result = 0;
+    //created a while loop and used Math.random to return a random number instead of 0
+    while(result === 0) {
+        result = Math.random();
+    }
+  return result;
+
+  // Only change code above this line.
+}
