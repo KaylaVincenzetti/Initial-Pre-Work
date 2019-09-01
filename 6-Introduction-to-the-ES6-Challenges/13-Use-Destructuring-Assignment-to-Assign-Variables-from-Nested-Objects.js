24955de90af16bee1698cfd213c39d4c86770106
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 21:42:07 -0600

    got max of forecast.tomorrow and assigned it to maxOfTomorrow

---
+++ 6-Introduction-to-the-ES6-Challenges/13-Use-Destructuring-Assignment-to-Assign-Variables-from-Nested-Objects.js
@@ -0,0 +1,17 @@
+//like in the previous exercise we can destructure nested objects into variables
+
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  // change code below this line
+  //got max of forecast.tomorrow and assigned it to maxOfTomorrow
+  const { tomorrow: { max: maxOfTomorrow } } = forecast; // change this line
+  // change code above this line
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
