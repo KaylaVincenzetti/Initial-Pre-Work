95d53a686505cc9bec21257f6a809d7c0d63db62
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 20:10:17 -0600

    used multiple conditional operators to check if number is positive, negative or zero

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,7 @@
+//used multiple conditional operators to check if number is positive, negative or zero
+
+function checkSign(num) {
+   return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+}
+
+checkSign(10);
