1bed5a69f19a6f406c662bf8cb10defb3e1588fb
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 19:26:06 -0600

    converted input of str to interger using parseInt

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,9 @@
+//learned about the parseInt function, it turns a string into an integer
+
+function convertToInteger(str) {
+  //converted input of str to interger using parseInt
+  return parseInt(str);
+
+}
+
+convertToInteger("56");
