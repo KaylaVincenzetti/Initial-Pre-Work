e68e6268fe18f4b8944d7b0b2c2caea2cfcb140c
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 19:52:42 -0600

    set up a conditional operator to check to see if a is equal to b

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,8 @@
+//learned about the conditional operator aka the ternary operator which can be used as a one line if/else statement
+
+function checkEqual(a, b) {
+  //set up a conditional operator to check to see if a is equal to b
+ return a == b ? true : false;
+}
+
+checkEqual(1, 2);
