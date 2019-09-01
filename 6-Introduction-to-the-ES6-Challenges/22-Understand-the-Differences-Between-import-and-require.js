653227ab50dd309268d103dc67eb6be4afa60093
Kayla Vincenzetti <knvg14@gmail.com>
2019-09-01 00:10:59 -0600

    import statement to import capitalizeString from string_functions

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+//learned to import which allows us to choose which parts of a module or file to load into a given file
+
+"use strict";
+capitalizeString("hello!");
+//import statement to import capitalizeString from string_functions
+import { capitalizeString } from "string_functions"
