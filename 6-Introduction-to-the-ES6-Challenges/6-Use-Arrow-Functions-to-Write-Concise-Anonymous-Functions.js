e017776419b594fe9ea58acc1b3c0f41eb61a501
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 20:51:49 -0600

    changed to inline function

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,6 @@
+//learned about arrow functions and how to write them. These inline fucntions don't need to be named becaused we don't reuse them
+//changed to inline function
+const magic = () => {
+  "use strict";
+  return new Date();
+};
