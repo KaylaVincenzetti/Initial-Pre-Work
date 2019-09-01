b26a73930d0a977fbc5c719809843e7e766ea7f2
Kayla Vincenzetti <knvg14@gmail.com>
2019-08-31 19:42:03 -0600

    used parseInt with radix to convert binary number into integer

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,9 @@
+//learned about radix which is the second argument in parseInt function, it specifies the base number of a string, can be an integer between 2 - 36
+
+function convertToInteger(str) {
+
+return parseInt(str, 2);
+}
+
+
+convertToInteger("10011");
